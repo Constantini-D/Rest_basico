1e5637391d7f211de76f2d136a6a614252f98837
Douglas Bueno <douglas.bueno@birmind.com.br>
2021-05-24 10:33:06 -0300

    Projeto Finalizado

--- cursos/views.py
+++ cursos/views.py
@@ -2,17 +2,26 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Curso, Avaliacao
 from .serializers import CursoSerializer, AvaliacaoSerializer
 
 
 class CursoAPIView(APIView):
-
+    """
+    API do Douglas
+    """
     def get(self, request):
         cursos = Curso.objects.all()
         serializer = CursoSerializer(cursos, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CursoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class AvaliacaoAPIView(APIView):
@@ -20,3 +29,10 @@ class AvaliacaoAPIView(APIView):
     def get(self, request):
         avalicao = Avaliacao.objects.all()
         serializer = AvaliacaoSerializer()
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AvaliacaoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
